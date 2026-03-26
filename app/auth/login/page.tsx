cbf0a23912b7eb2f0674970b57ddb2198ec66846
JulioLafre <julio-lafre@hotmail.com>
2026-03-26 09:09:44 -0300

    feat: Finalizando autenticação e melhorias de telas

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,43 +1,41 @@
 "use client"
 
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
-import { authClient } from "@/lib/auth-client";
-import { signUpSchema } from "@/schemas/auth";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Loader2 } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { useTransition } from "react";
-import { Controller, useForm } from "react-hook-form";
-import { toast } from "sonner";
-import z from "zod";
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { authClient } from "@/lib/auth-client"
+import { loginSchema } from "@/schemas/auth"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Loader2 } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useTransition } from "react"
+import { Controller, useForm } from "react-hook-form"
+import { toast } from "sonner"
+import z from "zod"
 
-export default function SignUpPage() {
+export default function LoginPage() {
     const [isPending, startTransition] = useTransition();
+
     const router = useRouter()
 
     const form = useForm({
-        resolver: zodResolver(signUpSchema),
+        resolver: zodResolver(loginSchema),
         defaultValues: {
             email: "",
-            name: "",
             password: ""
         }
     })
 
     //TODO Change for axios with FastApi in Future
-    async function onSubmit(data: z.infer<typeof signUpSchema>) {
-
+    async function onSubmit(data: z.infer<typeof loginSchema>) {
         startTransition(async () => {
-            await authClient.signUp.email({
+            await authClient.signIn.email({
                 email: data.email,
-                name: data.name,
                 password: data.password,
                 fetchOptions: {
                     onSuccess: () => {
-                        toast.success("Account created sucessfully")
+                        toast.success("Logged out sucessfully")
                         router.push("/")
                     },
                     onError: (error) => {
@@ -51,26 +49,12 @@ export default function SignUpPage() {
     return (
         <Card>
             <CardHeader>
-                <CardTitle>Sign Up</CardTitle>
-                <CardDescription>Create an account to get started</CardDescription>
+                <CardTitle>Login</CardTitle>
+                <CardDescription>Get started right away!</CardDescription>
             </CardHeader>
             <CardContent>
                 <form onSubmit={form.handleSubmit(onSubmit)}>
                     <FieldGroup className="gap-y-4">
-                        <Controller
-                            name="name"
-                            control={form.control}
-                            render={({ field, fieldState }) => (
-                                <Field>
-                                    <FieldLabel>Full Name</FieldLabel>
-                                    <Input aria-invalid={fieldState.invalid} placeholder="Julio Laiber" {...field} />
-                                    {fieldState.invalid && (
-                                        <FieldError errors={
-                                            [fieldState.error]
-                                        } />
-                                    )}
-                                </Field>
-                            )} />
                         <Controller
                             name="email"
                             control={form.control}
@@ -105,8 +89,9 @@ export default function SignUpPage() {
                                 <span>Loading...</span>
                             </>
                         ) : (
-                            <span>Sign Up</span>
-                        )}</Button>
+                            <span>Log in</span>
+                        )}
+                        </Button>
                     </FieldGroup>
                 </form>
             </CardContent>
--- components/web/Navbar.tsx
+++ components/web/Navbar.tsx
@@ -1,48 +1,73 @@
+"use client";
+
 import Link from "next/link";
-import { buttonVariants } from "../ui/button";
+import { Button, buttonVariants } from "../ui/button";
 import { ThemeToggle } from "./Theme-toggle";
+import { useConvexAuth } from "convex/react";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
 
 export function Navbar() {
-    return (
-        <nav className="w-full py-5 flex items-center justify-between">
-            <div className="flex items-center gap-8 ">
-                <Link href={"/"}>
-                    <h1 className="text-3xl font-bold">
-                        Next<span className="text-blue-500">Pro</span>
-                    </h1>
-                </Link>
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  const router = useRouter()
 
-                <div className="flex items-center gap-2">
-                    <Link className={buttonVariants({
-                        variant: 'ghost'
-                    })} href={"/"}>
-                        Home
-                    </Link>
-                    <Link className={buttonVariants({
-                        variant: 'ghost'
-                    })}  href={"/blog"}>
-                        Blog
-                    </Link>
-                    <Link className={buttonVariants({
-                        variant: 'ghost'
-                    })}  href={"/create"}>
-                        Create
-                    </Link>
-                </div>
-            </div>
+  async function handleLogout() {
+    await authClient.signOut({
+        fetchOptions: {
+            onSuccess: () => {
+                toast.success("Logged out sucessfully")
+                router.push("/")
+            },
+            onError: (error) => {
+                toast.error(error.error.message)
+            }
+        }
+    });
+  }
 
+  return (
+    <nav className="w-full py-5 flex items-center justify-between">
+      <div className="flex items-center gap-8">
+        <Link href="/">
+          <h1 className="text-3xl font-bold">
+            Next<span className="text-blue-500">Pro</span>
+          </h1>
+        </Link>
 
-            <div className="flex items-center gap-2">
-                <Link className={buttonVariants()} href={"/auth/sign-up"}>
-                    Sign up
-                </Link>
-                <Link className={buttonVariants({
-                    variant: 'secondary'
-                })} href={"/auth/login"}>
-                    Login
-                </Link>
-                <ThemeToggle />
-            </div>
-        </nav>
-    )
+        <div className="flex items-center gap-2">
+          <Link className={buttonVariants({ variant: "ghost" })} href="/">
+            Home
+          </Link>
+          <Link className={buttonVariants({ variant: "ghost" })} href="/blog">
+            Blog
+          </Link>
+          <Link className={buttonVariants({ variant: "ghost" })} href="/create">
+            Create
+          </Link>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2">
+        {isLoading ? null : isAuthenticated ? (
+          <Button onClick={handleLogout}>
+            Logout
+          </Button>
+        ) : (
+          <>
+            <Link className={buttonVariants()} href="/auth/sign-up">
+              Sign up
+            </Link>
+            <Link
+              className={buttonVariants({ variant: "secondary" })}
+              href="/auth/login"
+            >
+              Login
+            </Link>
+          </>
+        )}
+        <ThemeToggle />
+      </div>
+    </nav>
+  );
 }
